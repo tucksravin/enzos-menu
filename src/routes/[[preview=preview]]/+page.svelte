2d5e45b156224f3b570c2ad85c6c46721ca7908f
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-05-12 12:06:20 -0700

    changed easing

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -2,6 +2,7 @@
 	import enzos from '$lib/assets/icons/logos/enzos.svg'
   import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
   import { onMount } from 'svelte';
+  import { quintOut } from 'svelte/easing';
   import { fly } from 'svelte/transition';
 
 	let { data } = $props();
@@ -12,13 +13,17 @@
 
 	$effect(()=> {data; content = data.page.data;})
 
-	let isFeatureActive = $state(true);
+
+
+	let isFeatureActive = $state(false);
 
 	let featuredIndex = $state(0);
 
+	let showStar = $derived(featuredIndex>content.menu_items.length-1)
+
 	const nextFeature = () => {
 		isFeatureActive = true;
-		if (featuredIndex < content.menu_items.length-1) {
+		if (featuredIndex < content.menu_items.length+content.featured_items.length-1) {
 			featuredIndex += 1;
 		} else {
 			featuredIndex = 0;
@@ -191,12 +196,13 @@
 <svelte:window bind:innerWidth={viewpoortWidth} />
 
 <button class='absolute top-2 right-2 z-20' onclick={()=>isFeatureActive=!isFeatureActive}><i class='fa-solid {isFeatureActive?'fa-toggle-on':'fa-toggle-off'} fa-2xl text-primary'></i></button>
+<button class='absolute bottom-2 right-2 z-20' onclick={nextFeature}><i class='fa-solid fa-arrow-right fa-2xl text-primary'></i></button>
 
 
 <div class='w-screen h-screen p-12 bg-white relative'>	
 	<div class='w-full h-full relative'>
 		{#if !isFeatureActive}
-			<div class='w-full h-full absolute bg-[#FFF6ED] flex flex-col justify-between py-8 px-16' in:fly={{x:'-100%'}} out:fly={{x:'100%'}}>
+			<div class='w-full h-full absolute bg-[#FFF6ED] flex flex-col justify-between py-8 px-16' in:fly={{x:'-100%', duration:1800, easing: quintOut}} out:fly={{x:'100%', duration:1800, easing: quintOut}}>
 				<div class='flex flex-col items-center gap-3'>
 					<svg xmlns="http://www.w3.org/2000/svg" width="340" height="35" viewBox="0 0 340 35" fill="none">
 						<path d="M3.66585 23.5949C6.06478 23.0995 8.45909 24.7293 8.9486 27.1572C9.45116 29.6497 7.8727 32.0663 5.44179 32.5683C2.97888 33.0769 0.591105 31.4795 0.0950683 29.0192C-0.414022 26.4943 1.17096 24.1101 3.66585 23.5949Z" fill="black"/>
@@ -280,8 +286,8 @@
 					<div>EST 2024</div>
 				</div>
 			</div>
-		{:else}
-			<div in:fly={{x:'-100%'}} out:fly={{x:'100%'}} class='w-full h-full absolute bg-[#FFF6ED] flex flex-row justify-between  pl-16'>
+		{:else if !showStar}
+			<div in:fly={{x:'-100%', duration:1800, easing: quintOut}} out:fly={{x:'100%', duration:1800, easing: quintOut}} class='w-full h-full absolute bg-[#FFF6ED] flex flex-row justify-between  pl-16'>
 				<div class='w-1/2 h-full flex flex-col justify-between py-8'>
 					<div class='flex flex-row gap-5 items-center'>
 						<div class='detailing'>DETAILING</div>
@@ -315,6 +321,48 @@
 					</div>
 				</div>
 				<PrismicImage field={content.menu_items[featuredIndex].image}  class='w-1/2 h-full object-cover'/>
+			</div>
+			{:else}
+			<div in:fly={{x:'-100%', duration:1800, easing: quintOut}} out:fly={{x:'100%', duration:1800, easing: quintOut}} class='w-full h-full absolute bg-[#FFF6ED] flex flex-row justify-between  pl-16'>
+				<div class='w-1/2 h-full flex flex-col justify-between py-8'>
+					<div class='flex flex-row gap-5 items-center'>
+						<div class='detailing'>DETAILING</div>
+						<div class='h-1.5 w-1.5 bg-black rounded-full'></div>
+						<h4>Add On Services</h4>
+					</div>
+					<div class='flex flex-row'>
+						<div class='h-16 relative translate-y-2'>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47 47" fill="none" class='w-[180%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+								<path d="M22.6711 1.22841C23.0675 0.640884 23.9325 0.640882 24.3289 1.2284L25.9149 3.57869C26.2391 4.05912 26.9025 4.1642 27.3593 3.80746L29.5939 2.0623C30.1526 1.62605 30.9752 1.89333 31.1707 2.57461L31.9527 5.29995C32.1126 5.85706 32.7111 6.162 33.2558 5.96388L35.9203 4.99467C36.5864 4.75239 37.2861 5.26078 37.2615 5.96913L37.1631 8.80276C37.143 9.382 37.618 9.85697 38.1972 9.83686L41.0309 9.73847C41.7392 9.71387 42.2476 10.4136 42.0053 11.0797L41.0361 13.7442C40.838 14.2889 41.1429 14.8874 41.7001 15.0473L44.4254 15.8293C45.1067 16.0248 45.374 16.8474 44.9377 17.4061L43.1925 19.6407C42.8358 20.0975 42.9409 20.7609 43.4213 21.0851L45.7716 22.6711C46.3591 23.0675 46.3591 23.9325 45.7716 24.3289L43.4213 25.9149C42.9409 26.2391 42.8358 26.9025 43.1925 27.3593L44.9377 29.5939C45.374 30.1526 45.1067 30.9752 44.4254 31.1707L41.7001 31.9527C41.1429 32.1126 40.838 32.7111 41.0361 33.2558L42.0053 35.9203C42.2476 36.5864 41.7392 37.2861 41.0309 37.2615L38.1972 37.1631C37.618 37.143 37.143 37.618 37.1631 38.1972L37.2615 41.0309C37.2861 41.7392 36.5864 42.2476 35.9203 42.0053L33.2558 41.0361C32.7111 40.838 32.1126 41.1429 31.9527 41.7001L31.1707 44.4254C30.9752 45.1067 30.1526 45.374 29.5939 44.9377L27.3593 43.1925C26.9025 42.8358 26.2391 42.9409 25.9149 43.4213L24.3289 45.7716C23.9325 46.3591 23.0675 46.3591 22.6711 45.7716L21.0851 43.4213C20.7609 42.9409 20.0975 42.8358 19.6407 43.1925L17.4061 44.9377C16.8474 45.374 16.0248 45.1067 15.8293 44.4254L15.0473 41.7001C14.8874 41.1429 14.2889 40.838 13.7442 41.0361L11.0797 42.0053C10.4136 42.2476 9.71387 41.7392 9.73847 41.0309L9.83686 38.1972C9.85697 37.618 9.382 37.143 8.80276 37.1631L5.96913 37.2615C5.26079 37.2861 4.75239 36.5864 4.99467 35.9203L5.96388 33.2558C6.162 32.7111 5.85706 32.1126 5.29995 31.9527L2.5746 31.1707C1.89332 30.9752 1.62605 30.1526 2.0623 29.5939L3.80746 27.3593C4.1642 26.9025 4.05912 26.2391 3.57869 25.9149L1.22841 24.3289C0.640884 23.9325 0.640882 23.0675 1.2284 22.6711L3.57869 21.0851C4.05912 20.7609 4.1642 20.0975 3.80746 19.6407L2.0623 17.4061C1.62605 16.8474 1.89333 16.0248 2.57461 15.8293L5.29995 15.0473C5.85706 14.8874 6.162 14.2889 5.96388 13.7442L4.99467 11.0797C4.75239 10.4136 5.26078 9.71387 5.96913 9.73847L8.80276 9.83686C9.382 9.85697 9.85697 9.382 9.83686 8.80276L9.73847 5.96913C9.71387 5.26078 10.4136 4.75239 11.0797 4.99467L13.7442 5.96388C14.2889 6.162 14.8874 5.85706 15.0473 5.29995L15.8293 2.5746C16.0248 1.89332 16.8474 1.62605 17.4061 2.0623L19.6407 3.80746C20.0975 4.1642 20.7609 4.05912 21.0851 3.57869L22.6711 1.22841Z" fill="#C42428"/>
+							</svg>
+							<div class='feature-number text-white relative z-10'>{'0'+(featuredIndex-content.menu_items.length+1)}</div>
+						</div>
+						
+						<div class='ml-10'>
+							<div class='feature-headline w-4/5'>{content.featured_items[featuredIndex-content.menu_items.length].name}</div>
+							<div class='feature-body mt-11 pr-11'><PrismicRichText field={content.featured_items[featuredIndex-content.menu_items.length].description} /></div>
+							<div class='flex flex-row h-16 gap-11 mt-11'>
+								<div class='flex mt-2'>
+									<svg xmlns="http://www.w3.org/2000/svg" width="25" height="35" viewBox="0 0 25 35" fill="none">
+										<path d="M25 2.99107C25 3.61607 24.6491 4.33036 23.9474 5.04464C22.807 6.29464 21.3158 8.08036 19.693 10.1786C19.9561 10.7589 20.2193 11.5179 20.2193 12.1875C20.2193 13.7946 18.6404 17.1429 17.2368 17.1429C16.1842 17.1429 15.5263 15.1786 15.5263 14.3304C15.5263 12.2321 17.2807 11.6964 17.2807 10.5804C17.2807 10.1786 17.0175 9.95536 16.5789 9.95536C14.7807 9.95536 11.4474 13.6607 11.4474 15.4018C11.4474 17.7679 15.3509 20.6696 15.3509 23.4375C15.3509 25.9821 12.3684 27.2321 10.2193 27.2321C9.38597 27.2321 8.46491 27.0982 7.58772 26.7857C5.17544 30.2232 3.33333 32.9464 2.7193 33.9286C2.23684 34.7321 1.84211 35 1.49123 35C0.657895 35 0 33.125 0 32.4554C0 31.9196 0.175439 31.4286 0.526316 30.9375C0.964912 30.4018 2.45614 28.125 4.47368 25.0446C2.19298 23.3482 1.22807 21.7857 1.22807 20.7589C1.22807 20.0446 1.71053 19.6429 2.36842 19.6429C3.46491 19.6429 4.82456 20.7143 5 21.9196C5.26316 23.6607 7.23684 24.9554 8.42105 24.9554C9.69298 24.9107 11.0088 24.2857 11.0088 22.8125C11.0088 22.0536 10.614 21.2946 9.91228 20.625C8.77193 19.6429 7.0614 17.6786 7.0614 14.7768C7.0614 10.8036 11.886 5.89286 15.614 5.89286C16.0965 5.89286 16.4912 6.02679 16.886 6.29464C18.5526 3.88393 20 1.875 20.9649 0.625C21.3596 0.178569 21.6667 0 21.9737 0C22.9825 0 25 1.91964 25 2.99107Z" fill="black"/>
+									  </svg>
+									<div class='feature-cost -ml-1 -mt-5'>{content.featured_items[featuredIndex-content.menu_items.length].cost}</div>
+								</div>
+								<div class='h-full w-1 bg-primary translate-y-4'></div>
+								<div class='feature-time translate-y-5'>{content.featured_items[featuredIndex-content.menu_items.length].time}</div>
+
+							</div>
+						</div>
+					</div>
+					<div class='flex flex-row items-center justify-center gap-11 footer'>
+						<div>BOISE, ID</div>
+						<div class='h-1.5 w-1.5 bg-black rounded-full'></div>
+						<img src={enzos} alt='enzos logo' class='h-18' />
+						<div class='h-1.5 w-1.5 bg-black rounded-full'></div>
+						<div>EST 2024</div>
+					</div>
+				</div>
+				<PrismicImage field={content.featured_items[featuredIndex-content.menu_items.length].image}  class='w-1/2 h-full object-cover'/>
 			</div>
 		{/if}
 	</div>
